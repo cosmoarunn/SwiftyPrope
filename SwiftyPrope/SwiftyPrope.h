14f737db0671e9c69759b46f906c210af8954bd0
FGBS UK <aps@fgbs.uk>
2024-12-17 01:05:26 +0000

    Initial Commit

---
+++ SwiftyPrope/SwiftyPrope.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyPrope.h
+//  SwiftyPrope
+//
+//  Created by ARUN PANNEERSELVAM on 17/12/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyPrope.
+FOUNDATION_EXPORT double SwiftyPropeVersionNumber;
+
+//! Project version string for SwiftyPrope.
+FOUNDATION_EXPORT const unsigned char SwiftyPropeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyPrope/PublicHeader.h>
+
+
